fd21efc1199a63dcc3c0928874baea30a59b7ad4
LIYongJian <liyongjian-0603@qq.com>
2026-06-15 15:19:54 +0800

    阶段一

--- Assets/Scripts/Login/Panel/ChooseServerPanel.cs
+++ Assets/Scripts/Login/Panel/ChooseServerPanel.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.U2D;
 using UnityEngine.UI;
 
 public class ChooseServerPanel : BasePanel
@@ -58,8 +59,47 @@ public class ChooseServerPanel : BasePanel
 
         //显示自己时
         //应该初始化 上一次选择的服务器
-
         //更新当前选择
+        int id = LoginMgr.Instance.LoginData.frontServerID;
+        if (LoginMgr.Instance.LoginData.frontServerID <= 0)
+        {
+            txtName.text = "无";
+            imaState.gameObject.SetActive(false);
+
+        }
+        else 
+        {
+            //根据上一次登录的 服务器id 获取到 服务器信息
+            ServerInfo info = LoginMgr.Instance.ServerData[id - 1];
+            //为什么减一因为ServerData是List，而id只是不同的int
+            //当id=1时意味这上次选了第一个服务器这个服务器在ServerData[0]所以要减一
+            txtName.text = info.id + "区  " + info.name;
+            //一开状态图 显示
+            imaState .gameObject.SetActive(true);
+            //状态 
+            //加载图集
+            SpriteAtlas sa = Resources.Load<SpriteAtlas>("Login");
+            switch (info. state) 
+            {
+               case 0:
+                    imaState .gameObject .SetActive (false );
+               break;
+                case 1://流畅
+                    imaState.sprite = sa.GetSprite("ui_DL_liuchang_01");
+                    break;
+                case 2://繁忙
+                    imaState.sprite = sa.GetSprite("ui_DL_fanhua_01");
+                    break;
+                case 3://火爆
+                    imaState.sprite = sa.GetSprite("ui_DL_huobao_01");
+                    break;
+                case 4://维护
+                    imaState.sprite = sa.GetSprite("ui_DL_weihu_01");
+                    break;
+            }
+        }
+        //更新当前选择区间
+        UpdataPanle(1, 5 > LoginMgr.Instance.ServerData.Count ? LoginMgr.Instance.ServerData.Count : 5);
 
     }
  /// <summary>
@@ -69,7 +109,33 @@ public class ChooseServerPanel : BasePanel
  /// <param name="endIndex"></param>
     public void UpdataPanle(int beginIndex ,int endIndex) 
     {
-    
+        
+        //更新服务区间显示
+        txtRange.text = "服务器 "+beginIndex + "-"+endIndex;
+        //第一步删出之前的单个按钮
+        for (int i = 0; i <itemList .Count; i++)
+        {
+            //删除之前的 对象
+            Destroy(itemList[i]); 
+        }
+        //清空列表
+        itemList.Clear();
+
+        //第二步：创建新的按钮
+        for (int i = beginIndex; i <= endIndex; i++) 
+        {
+            //首先获取 服务器信息
+            ServerInfo nowInfo = LoginMgr.Instance.ServerData[i - 1];
+
+            //动态创建预制体
+            GameObject serverItem = Instantiate(Resources.Load<GameObject>("UI/ServerRightitem"));
+            serverItem.transform.SetParent(svRight.content,false );
+
+
+            //根据信息 更新按钮数据
+          //  ServerRightItem rightItem = serverItem.GetComponent<ServerRightItem>();
+        }
+
     }
 
 }
